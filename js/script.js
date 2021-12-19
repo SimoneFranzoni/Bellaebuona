81f743924457b0345d287f35e115a3a541edbdbe
SimoneFranzoni <simofranzoni99@gmail.com>
2021-12-19 22:51:18 +0100

    Aggiunta pagina eventi

--- js/script.js
+++ js/script.js
@@ -2,26 +2,20 @@
 const app = new Vue({
 
     el: '#app',
+
     data:{
-        images: ['./img/photo_section/1.jpg','./img/photo_section/1.jpg','./img/photo_section/2.jpg','./img/photo_section/3.jpg','./img/photo_section/4.jpg'],
-        counter: 0
+        menu_close: false,
+        
     },
 
     methods:{
-        nextSlide(){
-            this.counter++;
-            if(this.counter > this.images.length - 1){
-                this.counter = 0;
+
+        toggleMenu(){
+            if(this.menu_close === true){
+                this.menu_close = false;
+            } else {
+                this.menu_close = true;
             }
-            console.log(this.counter);
-        },
-    
-        prevSlide(){
-            this.counter--;
-            if(this.counter <= 0){
-                this.counter = this.images.length - 1;
-            }
-            console.log(this.counter);
         }
     }
 })
